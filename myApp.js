626bdec4649c52e9b44a97abd50596f466c3603a
Jessica Isoton <jessicasampaio195@gmail.com>
2021-06-12 21:07:01 +0200

    added env file and variable

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 const absolutePath = __dirname + "/views/index.html";
-
+const dotenv = require('dotenv');
+dotenv.config();
 
 // Send string as response
 /* const responseString = function (req, res) {
@@ -19,8 +20,14 @@ app.get("/", function (req, res) {
 //Serve Static Assets
 app.use("/public", express.static(__dirname + "/public"));
 
+console.log(process.env.MESSAGE_STYLE);
+
 app.get("/json", function (req, res) {
-  res.json({ "message": "Hello json" });
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ "message": "HELLO JSON" });
+  } else {
+    res.json(process.env.MESSAGE_STYLE);
+  }
 });
 
 module.exports = app;
