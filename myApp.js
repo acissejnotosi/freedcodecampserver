cd1cb0a8b69173d8c6a8285431f034ffcbfa0dad
Jessica Isoton <jessicasampaio195@gmail.com>
2021-06-13 10:41:44 +0200

    created middleware

--- myApp.js
+++ myApp.js
@@ -5,6 +5,11 @@ const absolutePath = __dirname + "/views/index.html";
 const dotenv = require('dotenv');
 dotenv.config();
 
+app.use(function (req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 // Send string as response
 /* const responseString = function (req, res) {
   res.send('Hello Express');
